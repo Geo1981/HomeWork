777acc4e31f85a0914827debba0dda50ba2f3b47
Geo1981 <georgii81@gmail.com>
2021-09-08 18:29:52 +0300

    Initial commit

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,26 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign(String[] args) {
+
+    }
+
+    public static void printColor(String[] args) {
+
+    }
+
+    public static void compareNumbers(String[] args) {
+
+    }
+}
