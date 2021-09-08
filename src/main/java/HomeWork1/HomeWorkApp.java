b60ba2ccd3dadaa719cfae60502a4832ed2780e4
Geo1981 <georgii81@gmail.com>
2021-09-08 19:36:33 +0300

    HomeWorkApp

--- src/main/java/HomeWork1/HomeWorkApp.java
+++ src/main/java/HomeWork1/HomeWorkApp.java
@@ -3,26 +3,57 @@ package HomeWork1;
 public class HomeWorkApp {
 
     public static void main(String[] args) {
+        System.out.println("printThreeWords:");
         printThreeWords();
+        System.out.println("\ncheckSumSign:");
+        checkSumSign();
+        System.out.println("\nprintColor:");
+        printColor();
+        System.out.println("\ncompareNumbers:");
+        compareNumbers();
 
     }
 
     public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        System.out.println("\tOrange");
+        System.out.println("\tBanana");
+        System.out.println("\tApple");
 
     }
 
-    public static void checkSumSign(String[] args) {
+    public static void checkSumSign() {
+        int a = -5;
+        int b = 1;
+
+        if (a + b >= 0) {
+            System.out.println("\tСумма положительная");
+        }
+        if (a + b < 0) {
+            System.out.println("\tСумма отрицательная");
+        }
 
     }
 
-    public static void printColor(String[] args) {
+    public static void printColor() {
+        int value = 10;
 
+        if (value <= 0) {
+            System.out.println("\tКрасный");
+        } else if (value > 0 & value <= 100 ) {
+            System.out.println("\tЖелтый");
+        } else {
+            System.out.println("\tЗеленый");
+        }
     }
 
-    public static void compareNumbers(String[] args) {
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 1;
 
+        if(a >= b) {
+            System.out.println("\ta >= b");
+        } else {
+            System.out.println("\ta < b");
+        }
     }
 }
